551f6b531e225d39e64f45e5ace3246d3249da46
syed <syedhasnain7456@gmail.com>
2023-11-22 14:39:46 +0530

    message

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
+const path = require('path');
 const app = express();
 app.use('/images', express.static(path.join(__dirname, 'uploads1')));
 const PORT = process.env.PORT || 4000;
