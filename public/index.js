e1f85d7561f6e66f9534056b4bdeddb6da1de962
syed <syedhasnain7456@gmail.com>
2023-11-23 14:26:17 +0530

    change

--- public/index.js
+++ public/index.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const multer = require('multer');
+
 const { scrapeLogic } = require("./scrapeLogic");
 const path = require('path');
 const app = express();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Set the directory where you want to store the uploaded images
+    cb(null, './uploaded_images');
+  },
+  filename: function (req, file, cb) {
+    // Rename the file if needed; you can adjust the naming logic here
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Endpoint to handle image uploads
+app.post('/upload', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  // If the file is uploaded successfully, send a success message
+  res.send('File uploaded successfully.');
+});
+
 app.use('/images', express.static(path.join(__dirname, 'uploads1')));
 const PORT = process.env.PORT || 4000;
 console.log(path.join(__dirname, ''));
